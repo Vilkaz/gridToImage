cee48c2e0423bd9978628f0cfe2fed3ab810b95b
Vilkaz <admin@vilkas.de>
2016-08-26 16:27:46 +0200

    grid steht, jetzt ab zur überprüfung

--- web/resources/js/grid.js
+++ web/resources/js/grid.js
@@ -1,6 +1,7 @@
 /**
  * Created by vkukanauskas on 26/08/2016.
  */
+
 function makeGrid() {
     var pictureContainer = $('#picture');
     var canvasContainer = $('#canvasContainer');
@@ -8,12 +9,12 @@ function makeGrid() {
     this.width = pictureContainer.width();
     this.height = pictureContainer.height();
     var amountOfVerticalLines = $('#linesVertical').val();
-    var verticalStep = this.width/amountOfVerticalLines;
+    var verticalStep = width/amountOfVerticalLines;
     var amountOfHorizontalLines = $('#linesHorizantal').val();
-    var horizontalStep = this.height/amountOfHorizontalLines;
+    var horizontalStep = height/amountOfHorizontalLines;
     canvasContainer.empty();
     canvasContainer.append(canvasDiv);
-    drawGridOnCanvas();
+    drawGridOnCanvas(this);
 
     function getCanvas() {
         var canvas = $('<canvas/>');
@@ -27,12 +28,13 @@ function makeGrid() {
         return canvas;
     }
 
-    function drawGridOnCanvas() {
+    function drawGridOnCanvas(obj) {
         var c = document.getElementById("canvasID");
         var ctx = c.getContext("2d");
         drawVerticlaLines(ctx);
         drawHorizontalLines(ctx);
         ctx.stroke();
+        drawRectangle(ctx);
     }
 
     function drawVerticlaLines(ctx) {
@@ -46,6 +48,43 @@ function makeGrid() {
             ctx.moveTo(0, y);
             ctx.lineTo(this.width, y);
         }
+    }
+
+    function drawRectangle(ctx){
+        var xWidth = pictureContainer.width();
+        var yHeight = pictureContainer.height();
+        var amountOfVerticalLines = $('#linesVertical').val();
+        var verticalStep = xWidth/amountOfVerticalLines;
+        var amountOfHorizontalLines = $('#linesHorizantal').val();
+        var horizontalStep = height/amountOfHorizontalLines;
+        for (var xx = 0;xx<this.width;xx+=verticalStep){
+            var lineReport="";
+            for(var yy=0;yy<this.height;yy+=horizontalStep){
+                if (isRectangleEmpty(ctx, xx,yy)){
+                    lineReport+="_";
+                }else {
+                    lineReport+="X"
+                };
+            }
+            console.log(lineReport);
+        }
+}
+
+    function isRectangleEmpty(ctx, x,y){
+        var pixels = ctx.getImageData(x, y, horizontalStep, verticalStep).data;
+        var empty = true;
+        $.each(pixels, function(iterator){
+            if (pixels[iterator]!=0){
+                empty=false;
+            };
+        })
+        return empty;
+    };
+
+    function rgbToHex(r, g, b) {
+        if (r > 255 || g > 255 || b > 255)
+            throw "Invalid color component";
+        return ((r << 16) | (g << 8) | b).toString(16);
     }
 
 }
