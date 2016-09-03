a90c173a917afc5583d1d6aa6eb463071834b53e
Vilkaz <admin@vilkas.de>
2016-09-03 23:20:35 +0200

    alten aufruf gelöscht.

--- web/resources/js/myImage.js
+++ web/resources/js/myImage.js
@@ -2,22 +2,12 @@
  * Created by vkukanauskas on 26/08/2016.
  */
 function getBinaryOnClick() {
-    // var param = {
-    //     pictureContainerID: "picture",
-    //     canvasContainerId: "canvasContainer",
-    //     amountOfVerticalLines: $('#linesVertical').val(),
-    //     amountOfHorizontalLines: $('#linesHorizantal').val(),
-    //     accuracy: 1
-    // }
-    // var bynariArray = binaryArrayGenerator.getBinaryListFromImage(param);
-
-
-    var param2 = {
+    var param = {
         imageURL: "http://localhost:8080/javax.faces.resource/img/test4Corners.png.xhtml",
         gridPixelWidth: 10,
-        gridPixelHeight: 20
+        gridPixelHeight: 10
     }
-    var array = imageScanner.getBinaryListFromImage(param2)
+    var array = imageScanner.getBinaryListFromImage(param)
     console.log(array);
 
 }
